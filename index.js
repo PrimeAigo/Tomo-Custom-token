ece7a65faf7beff0a9a230867d1cb9a2881148c2
PrimeAigo <138360196+PrimeAigo@users.noreply.github.com>
2023-07-10 15:00:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 const express = require('express');
-const Web3 = require('web3')
-const web3 = new Web3('https://rpc.tomochain.com')
+const Web3 = require('web3');
+const web3 = new Web3('https://rpc.tomochain.com');
 
-const abi = require('./rrr.json')
+const abi = require('./rrr.json');
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
-  const { privateKey, address } = web3.eth.accounts.create()
-  res.json({ privateKey, address })
-})
+  const { privateKey, address } = web3.eth.accounts.create();
+  res.json({ privateKey, address });
+});
 
 app.get('/sendkeraluda/:amount/:privateKey/:toAddress/:contractAddress', async (req, res) => {
-const { amount, privateKey, toAddress, contractAddress } = req.params
-const contractInstance = new web3.eth.Contract(abi, contractAddress)
-const gasPrice = await web3.eth.getGasPrice()
-const data = contractInstance.methods.transfer(toAddress, `${amount}000000000000000000`).encodeABI();
-const tx = {
-from: await web3.eth.accounts.privateKeyToAccount(privateKey).address,
-to: contractAddress,
-data,
-gas: 216440,
-gasPrice,
-chainId: 88,
-}
+  const { amount, privateKey, toAddress, contractAddress } = req.params;
+  const contractInstance = new web3.eth.Contract(abi, contractAddress);
+  const gasPrice = await web3.eth.getGasPrice();
+  const data = contractInstance.methods.transfer(toAddress, `${amount}000000000000000000`).encodeABI();
+  const tx = {
+    from: await web3.eth.accounts.privateKeyToAccount(privateKey).address,
+    to: contractAddress,
+    data,
+    gas: 216440,
+    gasPrice,
+    chainId: 88,
+  };
 
-try {
-const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey)
-const sentTx = await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
-res.json({ from: tx.from, to: toAddress, amount, txHash: sentTx.transactionHash })
-} catch (error) {
-res.status(500).json({ error: error.message })
-}
-})
+  try {
+    const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
+    const sentTx = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+    res.json({ from: tx.from, to: toAddress, amount, txHash: sentTx.transactionHash });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
